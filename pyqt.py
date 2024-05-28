5b476a0d731ce9c3254c0c143f26bd53c4c05a89
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2024-05-28 19:47:57 +0900

    Update pyqt.py

--- pyqt.py
+++ pyqt.py
@@ -1,15 +1,17 @@
 import sys
 from PyQt5.QtWidgets import *
-
+# 무조건 두 칸 띄우기
 
 class Main(QMainWindow) :
     def __init__(self):
         super(Main, self).__init__()
 
         self.label = QLabel("안녕하세요", self)
-
+# 무조건 두 칸 띄우기
 
 app = QApplication(sys.argv)
 window = Main()
 window.show()
 app.exec_()
+
+# PyQt위젯 기본 세팅
